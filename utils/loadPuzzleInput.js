ca94e2514ab6a0a23dc7b33d58a9e65f545b88d4
Reniat <reniat314@gmail.com>
2021-12-13 21:47:10 -0600

    adding crlf support in loadpuzzleinput

--- utils/loadPuzzleInput.js
+++ utils/loadPuzzleInput.js
@@ -2,7 +2,7 @@ import fs from "fs";
 
 const loadPuzzleInput = (dayNumber, splitString = "\n") => {
   const fileString = fs.readFileSync(`./Day${dayNumber}/input.txt`, { encoding: "utf8", flag: "r" });
-  return fileString.split(splitString);
+  return fileString.replace(/\r\n/g, "\n").split(splitString);
 };
 
 export default loadPuzzleInput;
